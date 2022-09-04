d5ae0b72723740295098bb6116e6d70bb4eb5e2c
Rishabh Ranjan <rishabh9050@gmail.com>
2022-09-04 20:22:06 +0530

    Check string for Palindrome

--- palindromeString.java
+++ palindromeString.java
@@ -1,19 +1,19 @@
 public class palindromeString {
     public static void main(String args[]) {
-        String s = "abcba";
-        if (isPalindrome(s))
+        String s = "abcba"; // Sample string for testing
+        if (isPalindrome(s)) // If string is palindrom, print the statement
             System.out.println("Palindrome");
 
     }
 
     public static boolean isPalindrome(String str) {
-        StringBuilder rev = new StringBuilder();
+        StringBuilder rev = new StringBuilder(); //Using StringBuilder instead of String for better time complexity
 
-        for (int i = str.length() - 1; i >= 0; i--) {
-            rev.append(str.charAt(i));
+        for (int i = str.length() - 1; i >= 0; i--) { // Traverse string from end to beginning
+            rev.append(str.charAt(i)); // append character at i index into rev
         }
 
-        if (str.equals(rev.toString())) {
+        if (str.equals(rev.toString())) { // Convert StringBuilder rev to String and return true if equal to str else return false
             return true;
         } else {
             return false;
